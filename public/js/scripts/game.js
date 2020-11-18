decc10beb3aac4e88a1f4c0199a0dbea499d3cc5
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-11-18 20:05:42 -0300

    Voltando no primeiro commit

--- public/js/scripts/game.js
+++ public/js/scripts/game.js
@@ -34,7 +34,7 @@ pointsOnScreen.forEach(point => {
             setTimeout(() => changeFill(selectedPoint, '#EF3535'), 1000)
         }
         else {
-            currentPlayer.grade > 0 && currentPlayer.grade-- 
+            currentPlayer.grade--  
 
             setSound('wrong')
             changeFill(selectedPoint, '#B13737')
@@ -63,8 +63,8 @@ pointsOnScreen.forEach(point => {
             currentPlayer.setEvaluate()
             
             const [ domain ] = window.location.href.split('game')
-            const path = `result`
-            const attr = currentPlayer.toParam()
+            const path = `results.html?`
+            const attr = currentPlayer.toString()
 
             window.location.href = `${domain}${path}${attr}`
         }
